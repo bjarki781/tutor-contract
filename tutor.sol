19a0a6c75e06e1b9bb44d636c526a3a1d592b2d8
bjarki781 <bjg60@hi.is>
2024-07-28 15:25:45 +0000

    make flow clearer

--- tutor.sol
+++ tutor.sol
@@ -2,6 +2,8 @@
 pragma solidity 0.8.26;
 
 contract Oracle {
+  // this contract should communicate with an external server
+  // sending a students public key and receiving an encrypted random question/answer pair
     string[] public questions = [ "The answer to this question is 2"
                                 , "The answer to this question is 34"
                                 ];
@@ -10,9 +12,9 @@ contract Oracle {
                              ];  
 }
 
-
+// each student has a separate contract deployed for them 
+// tracking their progress and grade
 contract TutorContract {
-  // is this an optimal size?
   type fxp is int64;
 
   Oracle TutorOracle; 
@@ -27,9 +29,11 @@ contract TutorContract {
   event Answered(bool correct);
 
   constructor(address OracleAddress) payable {
+      // here we want to decrypt the encrypted questions and answers sent from the oracle
       TutorOracle = Oracle(OracleAddress);
       questions = [TutorOracle.questions(0), TutorOracle.questions(1)];
       answers = [TutorOracle.answers(0), TutorOracle.answers(1)];
+
       weights = [fxp.wrap(2000000), fxp.wrap(1500000), fxp.wrap(900000), fxp.wrap(400000), fxp.wrap(200000)];
       points = [fxp.wrap(1000000), fxp.wrap(1000000), fxp.wrap(1000000), fxp.wrap(1000000), fxp.wrap(1000000)];
       progress = 0;
@@ -65,20 +69,23 @@ contract TutorContract {
   }
 
   function get_current_question() external  view returns (string memory) {
+      // this data should be specific to the student asking the question
       return questions[progress];
   }
 
    function get_progress() external  view returns (uint8) {
       return progress;
   }
-
-  function answer(uint8 a, address payable to) external payable  {
+  // returns correct answer
+  function answer(uint8 a, address payable to) external payable returns (uint8) {
       require(msg.value == 0 wei);
 
       if (a != answers[progress]) {
 	      points = push_front(points, fxp.wrap(-2000000));
 	      points.pop();
 	      emit Answered(false);
+
+        return answers[progress];
       }
 
       // else if it was correct
@@ -95,8 +102,10 @@ contract TutorContract {
       if (fxp.unwrap(grade) > 9750000) {
         (bool sent, ) = to.call{value: 500 wei}("");
         require(sent, "Failed to send Ether");
-      }
+      } 
+      return answers[progress];
     }
-  
+
+   
 }
 
